9e1f4f74223002030b71bd9e7df026a7f8a1ceb4
gray <wotjs08@hanmail.net>
2019-01-30 02:50:28 +0900

    no message

--- 01_HelloSpring/src/main/java/com/kh/spring/common/interceptor/LoggerInterceptor.java
+++ 01_HelloSpring/src/main/java/com/kh/spring/common/interceptor/LoggerInterceptor.java
@@ -12,6 +12,10 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
 
 	// 오버라이딩 할 메소드가 3개 정도 있다. (preHandle, postHandle, afterCompletion)
 	// 어댑터는 오버라이딩을 미리 해놔서 하고 싶은 것만 하라고 하는 클래스이다.
+	
+	// controller로 보내기 전에 처리하는 인터셉터
+	// 반환이 false라면 controller로 요청을 안함
+	// 매개변수 Object는 핸들러 정보를 의미한다. ( RequestMapping , DefaultServletHandler )
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -22,7 +26,8 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
 		}
 		return super.preHandle(request, response, handler);
 	}
-
+	
+	// controller의 handler가 끝나면 처리됨
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
@@ -30,7 +35,8 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
 			logger.debug("--------------- view --------------");
 		}
 	}
-
+	
+	// view까지 처리가 끝난 후에 처리됨
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
@@ -39,6 +45,5 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
 		}
 		super.afterCompletion(request, response, handler, ex);
 	}
-	
-	
+
 }
